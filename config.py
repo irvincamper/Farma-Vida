8931eff2650c198f5328def8c6f4c2193208566c
Irvin Hernandez <2430334@upt.edu.mx>
2025-11-27 21:07:15 -0600

    Repuse el commit

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ class Config:
     
     # ¡USA ESTA CLAVE! LA SECRETA.
     SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwaHR5dGV6ZHd3ZnB1dnd4dHBuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1Mzc2MjIwNywiZXhwIjoyMDY5MzM4MjA3fQ.o5hltHUTaKsf33y3Sg2K0xgxAVsuOeIBwxzRU9MNAQ4"
+    
+    # 🌟 ¡CORRECCIÓN! Debe ir aquí, dentro de la clase.
+    GEMINI_API_KEY = "AIzaSyAJL8fHA1MUF4tMpnb84lFPlmk-FUpT_4c"  
